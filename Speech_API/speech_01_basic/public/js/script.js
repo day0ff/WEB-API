5ccab48ed1a09c70de81b09cad1d3bf3822b69d9
day0ff <day85@tut.by>
2020-04-27 16:33:46 +0300

    Add saving speech in file.

--- Speech_API/speech_01_basic/public/js/script.js
+++ Speech_API/speech_01_basic/public/js/script.js
@@ -1,5 +1,6 @@
 window.onload = ()=>{
     const language = document.querySelector('select');
+    const save = document.querySelector('button');
     const content = document.querySelector('content');
 
     // TODO Implement check for support. Gecko is currently not support it.
@@ -34,5 +35,24 @@ window.onload = ()=>{
     language.addEventListener('change', ({target}) => {
         recognition.lang = target.value;
         recognition.stop();
-    })
+    });
+
+    save.addEventListener('click', () => {
+        const time = new Date().getTime().toString().slice(-6);
+        const data = Array.from(content.querySelectorAll('p')).map(p=>p.textContent).join('\n');
+        const blob = new Blob([data], { type: "text/plain;charset=utf-8" });
+        const a = document.createElement('a');
+        const url = URL.createObjectURL(blob);
+
+        a.href = url;
+        a.download = `speech${time}.txt`;
+        document.body.appendChild(a);
+        a.click();
+
+        setTimeout(()=> {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+
+    });
 };
