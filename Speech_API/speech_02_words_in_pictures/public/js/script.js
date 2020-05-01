d3ce119a22930758e2eaf9e1d2761c5540d48a11
day0ff <day85@tut.by>
2020-05-01 17:48:00 +0300

    Implement search images of spoken words.

--- Speech_API/speech_02_words_in_pictures/public/js/script.js
+++ Speech_API/speech_02_words_in_pictures/public/js/script.js
@@ -1,10 +1,45 @@
-const URL = 'http://www.google.com/search?q=$s+svg';
 window.onload = () => {
     const language = document.querySelector('select');
+    const article = document.querySelector('article');
 
-    language.addEventListener('change', ({target}) => {
-        console.log('language changed')
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+
+    recognition.interimResults = false;
+    recognition.lang = language.value;
+
+    let paragraph = document.createElement('p');
+
+    recognition.addEventListener('result', async({results}) => {
+        const words = Array.from(results)
+            .map(result=>result[0])
+            .map(result=>result.transcript);
+        paragraph.textContent = words.join('');
+        if(results[0].isFinal){
+            paragraph = document.createElement('p');
+            const img = document.createElement('img');
+            const {word, src} = await searchImage(words[0]);
+            img.src = src;
+            console.log(word, src);
+            paragraph.appendChild(img);
+            article.innerHTML = '';
+            article.appendChild(paragraph);
+        }
     });
 
-    fetch(URL.replace('$s','world')).then(response=>response.text()).then(text=>console.log(text));
+    recognition.addEventListener('end', recognition.start);
+
+    recognition.start();
+
+    language.addEventListener('change', ({target}) => {
+        recognition.lang = target.value;
+        recognition.stop();
+    });
+
 };
+
+function searchImage(word){
+    return fetch(`/img?word=${word}`)
+        .then(response => response.json());
+}
--- Speech_API/speech_02_words_in_pictures/src/app.js
+++ Speech_API/speech_02_words_in_pictures/src/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+const puppeteer = require('puppeteer');
+
 
 const PATH_PUBLIC_DIR = path.join(__dirname, '../public');
-
+const URL = 'https://yandex.by/images/search?text=%s+jpeg';
 
 const app = express();
 
 app.use(express.static(PATH_PUBLIC_DIR));
+
+app.get('/img', async (req, res) => {
+    const word = req.query.word;
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.goto(URL.replace('%s',word));
+    const src = await page.evaluate(() =>  document.querySelectorAll('img').item(1).src);
+    await browser.close();
+    res.send({word, src});
+});
 
 app.listen(3000, () => console.log('listening on port 3000\nhttp://localhost:3000/'));
