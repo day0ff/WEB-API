4e29ec62500bd20c63f7a596e90bf9b0ca5b1f75
day0ff <day85@tut.by>
2020-05-01 20:23:09 +0300

    Init Fairy Tale.

---
+++ Speech_API/speech_03_fairy_tale/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const PATH_PUBLIC_DIR = path.join(__dirname, '../public');
+
+const app = express();
+
+app.use(express.static(PATH_PUBLIC_DIR));
+
+app.listen(3000, () => console.log('listening on port 3000\nhttp://localhost:3000/'));
