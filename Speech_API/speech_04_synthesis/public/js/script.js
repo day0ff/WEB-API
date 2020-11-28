fc2980ac842b57c91a3df13d6e562b5b55b4b5a5
day0ff <day85@tut.by>
2020-11-28 21:56:26 +0300

    Implement speech synthesis example.

--- Speech_API/speech_04_synthesis/public/js/script.js
+++ Speech_API/speech_04_synthesis/public/js/script.js
@@ -1,11 +1,43 @@
-window.onload = ()=>{
+window.onload = async () => {
     const language = document.querySelector('select');
     const say = document.querySelector('button');
+    const textarea = document.querySelector('textarea');
+    const message = new SpeechSynthesisUtterance();
+    const voices = await getVoices()
+        .then(voices => voices.filter(voice => /(ru|en|fr|de)/i.test(voice.lang)));
 
+    language.innerHTML = getOptions(voices).join('');
+    message.voice = voices[0];
 
     say.addEventListener('click', () => {
-        const msg = new SpeechSynthesisUtterance('Hello World');
-
-        window.speechSynthesis.speak(msg);
+        if (textarea.value) {
+            message.voice = voices[language.value];
+            message.text = textarea.value;
+            window.speechSynthesis.speak(message);
+        }
     });
 };
+
+function getVoices() {
+    return new Promise((resolve) => {
+        let voices = speechSynthesis.getVoices();
+        if (voices.length) {
+            resolve(voices);
+            return;
+        }
+        speechSynthesis.onvoiceschanged = () => {
+            voices = speechSynthesis.getVoices();
+            resolve(voices);
+        }
+    })
+}
+
+function getOptions(languages) {
+    return languages.map((language, index) => {
+        const option = document.createElement('option');
+
+        option.value = index;
+        option.innerText = `${language.lang} - ${language.name}`;
+        return option.outerHTML;
+    });
+}
